7beb5b83ad0aa98f6d9b7cb26e363dd24f3dca1e
Zeyroxfame <133609170+Zeyroxfame@users.noreply.github.com>
2023-11-15 12:44:17 -0500

    Update script.js

--- Clicker/script.js
+++ Clicker/script.js
@@ -8,7 +8,7 @@ let multiplier = 1;
 
 function add1() {
 	counter++;
- money + 1 * multiplier;
+ money + 1;
 	const visual = `You clicked the button ${counter} times.`
  const moneyVisual = `${money}$`
 	document.getElementById("visual").innerHTML = visual
