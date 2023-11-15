695a587d520370cc86bb93cf3acb9279cbd40d83
Zeyroxfame <zeyroxfame@gmail.com>
2023-11-15 18:03:29 -0500

    UPGRADES

--- Clicker/script.js
+++ Clicker/script.js
@@ -4,15 +4,31 @@ function alert1() {
 
 let counter = 0;
 let money = 0;
-let multiplier = 2;
+let multiplier = 1;
+
+
 
 function add1() {
 	counter++;
- newMoney = money + 1 * multiplier;
- money = newMoney;
+    money = money + 1 * multiplier;
+    money = Math.round(money * 100) / 100;
+	const moneyVisual = `${money}\$`;
 	const visual = `You clicked the button ${counter} times.`;
- const moneyVisual = `${money}$`;
 	document.getElementById("visual").innerHTML = visual;
  document.getElementById("money").innerHTML = moneyVisual;
 }
+function incrementMultiplier() {
+	if (money >= 20) {
+		money = money - 20;
+		multiplier = multiplier * 1.1;
+		multiplier = Math.round(multiplier * 100) / 100;
+		money = Math.round(money * 100) / 100;
+		const moneyVisual = `${money}\$`;
+		const multiplierVisual = `Multiplier = ${multiplier}`;
+		document.getElementById("multiplier").innerHTML = multiplierVisual;
+		document.getElementById("money").innerHTML = moneyVisual;
+	}
+	else {document.getElementById("notEnough").innerHTML = "Not enough money."}
+}
+    	
 	
