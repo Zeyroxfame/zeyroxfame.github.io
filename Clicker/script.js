5777440f27d86027cafe98a03f0309e34067d35e
Zeyroxfame <133609170+Zeyroxfame@users.noreply.github.com>
2023-11-15 14:11:10 -0500

    Update script.js

--- Clicker/script.js
+++ Clicker/script.js
@@ -8,7 +8,7 @@ let multiplier = 1;
 
 function add1() {
 	counter++;
- money + 1;
+ money ++;
 	const visual = `You clicked the button ${counter} times.`;
  const moneyVisual = `${money}`;
 	document.getElementById("visual").innerHTML = visual;
