e639f8ae02b7147a0443fd58b1a588dcb31785e5
Zeyroxfame <zeyroxfame@gmail.com>
2023-11-28 21:58:21 -0500

    ;

--- New folder/script.js
+++ New folder/script.js
@@ -1,4 +1,11 @@
 function print() {
+	let input = document.getElementById("input");
 	let output = input.value;
+	let input2 = document.getElementById("input2");
+	let randomNumber = math.random();
+	
+	if (randomNumber > 50) {
+		
+	
 	document.getElementById("output").innerHTML = output;
 }
