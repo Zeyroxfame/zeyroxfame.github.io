b53ed4b478af1f1512e1055ca57f30d36d14fa14
Zeyroxfame <zeyroxfame@gmail.com>
2023-12-14 18:38:20 -0500

    Massive update
    d

--- Clicker/script.js
+++ Clicker/script.js
@@ -1,18 +1,29 @@
 let counter = 0;
 let money = 0;
 let multiplier = 1;
-
-
+let base = 1;
 
 function add1() {
 	counter++;
-    money = money + 1 * multiplier;
+    money = money + base * multiplier;
     money = Math.round(money * 100) / 100;
 	const moneyVisual = `${money}\$`;
 	const visual = `You clicked the button ${counter} times.`;
 	document.getElementById("visual").innerHTML = visual;
  document.getElementById("money").innerHTML = moneyVisual;
 }
+function debug() {
+confirm("Activate debug mode?");
+if (confirm("Activate debug mode?")) {
+	const node = document.createElement("button");
+	const textnode = document.createTextNode("+100000$");
+	node.appendChild(textnode);
+	document.getElementById("debug").appendChild(node);
+   }
+}
+//	money = money + 9999999;
+//	const moneyVisual = `${money}\$`;
+//	document.getElementById("money").innerHTML = moneyVisual;	
 function incrementMultiplier() {
 	if (money >= 20) {
 		money = money - 20;
@@ -20,11 +31,24 @@ function incrementMultiplier() {
 		multiplier = Math.round(multiplier * 100) / 100;
 		money = Math.round(money * 100) / 100;
 		const moneyVisual = `${money}\$`;
-		const multiplierVisual = `Multiplier = ${multiplier}`;
+        const multiplierVisual = `Multiplier = ${multiplier}`;
 		document.getElementById("multiplier").innerHTML = multiplierVisual;
 		document.getElementById("money").innerHTML = moneyVisual;
 	}
 	else {document.getElementById("notEnough").innerHTML = "Not enough money."}
 }
+function incrementBase() {
+    if (money >= 100) {
+	    money = money - 100;
+	    base = base + 1;
+	    money = Math.round(money * 100) / 100;
+	    const moneyVisual = `${money}\$`;
+	    const baseVisual = `Base = ${base}`;
+	    document.getElementById("base").innerHTML = baseVisual;  
+	    document.getElementById("money").innerHTML = moneyVisual;
+    }
+	else {document.getElementById(notEnough).innerHTML = "Not enough money."}
+}	
+	
     	
 	
