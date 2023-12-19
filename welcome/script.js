645bc583247fa1c4c0ee1d3d17ffa87ee3189ac5
Zeyroxfame <zeyroxfame@gmail.com>
2023-12-19 18:37:03 -0500

    v

--- welcome/script.js
+++ welcome/script.js
@@ -1,5 +1,5 @@
-function randomNumber {
-    const randomNumber;
+function RandomNumber() {
+    let randomNumber;
     const randomNumberVisual = `Your random number is ${randomNumber}`;
     randomNumber = Math.random();
     document.getElementById("output").innerHTML = randomNumberVisual;
