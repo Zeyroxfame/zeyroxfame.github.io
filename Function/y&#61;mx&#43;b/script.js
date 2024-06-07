154baefc3332aac4980b17f403d6eb5ce97e88ff
Zeyroxfame <zeyroxfame@gmail.com>
2024-06-06 21:14:22 -0400

    Styling
    Better navigation + menu looks better

--- Function/y&#61;mx&#43;b/script.js
+++
@@ -1,8 +0,0 @@
-function domath() {
-    let y = 2;
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("It worked! My time machine worked!");
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-}
